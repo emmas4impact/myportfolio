75d82030df8efb925044321e96d366b12ffbcdde
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-10-27 19:31:19 +0200

    create portfolio react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Heading from './components/Heading'
+import Skills from './components/Skills'
+import About from './components/About'
+import Projects from './components/Projects'
+import Portfolio from './components/Portfolio'
+import Footer from './components/Footer'
+import "animate.css/animate.min.css";
+
+function App() {
+  return (
+    <div>
+       <Heading />
+       <About />
+       <Projects />
+       <Skills />
+       <Portfolio />
+       <Footer />
+       
+    </div>
+  );
+}
+
+export default App;
