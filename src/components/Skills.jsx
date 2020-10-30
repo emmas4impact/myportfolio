65ee8a0ac2ac949a8fba7b2fe5c804d86770f5d7
Oksana <myzove@mail.ru>
2020-10-30 14:45:15 +0200

    changing color

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,7 +9,7 @@ const Skills = () => {
         AOS.init()
     }, [])
     return( 
-        <div style={{background:'rgba(151, 152, 153)', color: '#000'}}>
+        <div style={{background:'#fff', color: '#000'}}>
             <Container>
                 <Row>
                     <Col>
